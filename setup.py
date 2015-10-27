6c104daf0f56d11a8e4990d1d2be4c69435cedb2
e412646 <Daniel.Yeakley@Honeywell.com>
2015-10-26 20:13:30 -0400

    Updated version to 2.0.   Significant changes, may not be able  backwards compatible.
    Change-Id: Icaf334eb767df9a140ec3f20ded9632fb76b560a

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyhwtherm',
-    version='0.1',
+    version='2.0',
     license='MIT',
     author='Dan Yeakley',
     author_email='ddyeakley@gmail.com',
